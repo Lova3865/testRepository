9e388b648f17a0f7b0919ce1b6c98ad2060bde86
lova3865 <lova3865@gmail.com>
2017-04-28 12:23:28 +0900

    deleted

--- calendar.c
+++
@@ -1,9 +0,0 @@
-#include "diary.h"
-//this is Calendar()
-//test
-int calendar()
-{
-	printf("function calendar.\n");
-	return 0;
-}
-
--- memo.c
+++
@@ -1,7 +0,0 @@
-#include "diary.h"
-int memo()
-{
-	 printf("Function memo\n");
-	 return 0;
-}
-
